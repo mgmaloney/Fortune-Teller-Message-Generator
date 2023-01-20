7492f8151932df0089af91754290227ce874e8ff
Matthew Maloney <matthewgmaloney@gmail.com>
2023-01-20 13:00:25 -0600

    completed message funcitonality

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 let signs = ['Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius'];
-let celestialBody = ['Moon', 'Sun', 'Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto'];
-let messageContent = ['your future is uncertain', 'riches are coming to you', 'the stars are in your favor', 'don\'t make any big investments', 'avoid dark places', 'building relationships will bring you joy', 'you should get 3 cats'];
+let celestialBodies = ['Moon', 'Sun', 'Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto'];
+let messageContents = ['your future is uncertain', 'riches are coming to you', 'the stars are in your favor', 'don\'t make any big investments', 'avoid dark places', 'building relationships will bring you joy', 'you should get 3 cats'];
 
+function messageGenerator () {
+    let sign = signs[(Math.floor(Math.random() * signs.length))];
+    let celestialBody = celestialBodies[(Math.floor(Math.random() * celestialBodies.length))];
+    let messageContent = messageContents[(Math.floor(Math.random() * messageContents.length))];
+    let message = `${celestialBody} is in ${sign}, so ${messageContent}.`
+    console.log(message);
+}
+
+
+messageGenerator();
