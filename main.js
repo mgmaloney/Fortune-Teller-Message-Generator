35b23d4176de6a25373bf900fc1ae5dd701b17f5
Matthew Maloney <matthewgmaloney@gmail.com>
2023-01-20 12:34:24 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+let signs = ['Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius'];
+let celestialBody = ['Moon', 'Sun', 'Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto'];
+let messageContent = ['your future is uncertain', 'riches are coming to you', 'the stars are in your favor', 'don\'t make any big investments', 'avoid dark places', 'building relationships will bring you joy', 'you should get 3 cats'];
+
